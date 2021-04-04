cd7827b0e81c5d56f5ac8beb4d945e648052e06d
hideonbush233-lucy <55594771+hideonbush233-lucy@users.noreply.github.com>
2021-04-04 18:55:43 +0800

    Update JZ50-数组中的重复数字.py

--- 剑指Offer/JZ50-数组中的重复数字.py
+++ 剑指Offer/JZ50-数组中的重复数字.py
@@ -1,11 +1,10 @@
 # 在一个长度为n的数组里的所有数字都在0到n-1的范围内。 数组中某些数字是重复的，但不知道有几个数字是重复的。
 # 也不知道每个数字重复几次。请找出数组中任一一个重复的数字。 例如，如果输入长度为7的数组[2,3,1,0,2,5,3]，那么对应的输出是2或者3。存在不合法的输入的话输出-1
 
-#采用集合set元素不重复的属性，依次添加元素，如果集合列表长度不变，说明该元素重复了，返回即可
-#
-
 # @param numbers int整型一维数组 
 # @return int整型
+
+#采用集合set元素不重复的属性，依次添加元素，如果集合列表长度不变，说明该元素重复了，返回即可
 class Solution:
     def duplicate(self , numbers ):
         # write code here
@@ -18,3 +17,16 @@ class Solution:
             if length == len(res):
                 return numbers[i]
         return -1
+
+# 直接利用列表的count()函数统计出现次数
+def duplicate(self , numbers ):
+# write code here
+    if len(numbers) == 0:
+        return -1
+    res = []
+    for i in range(len(numbers)):
+        if numbers.count(numbers[i]) > 1:
+            return numbers[i]
+    return -1
+
+# 

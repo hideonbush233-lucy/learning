d3071fc08a23378dfa8a8e350ec8073752a28108
hideonbush233-lucy <55594771+hideonbush233-lucy@users.noreply.github.com>
2021-04-09 22:25:50 +0800

    Create JZ34-数组中只出现一次的两个数字.py

---
+++ 剑指Offer/JZ34-数组中只出现一次的两个数字.py
@@ -0,0 +1,13 @@
+# 一个整型数组里除了两个数字只出现一次，其他的数字都出现了两次。请写程序找出这两个只出现一次的数字。
+
+# 投机取巧的方法 直接用count()函数
+class Solution:
+    def FindNumsAppearOnce(self , array ):
+        # write code here
+        res = []
+        for item in array:
+            if array.count(item) == 1:
+                res.append(item)
+        return sorted(res)
+      
+  

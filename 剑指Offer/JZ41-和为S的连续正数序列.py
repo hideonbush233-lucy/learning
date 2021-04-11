caef2e9928111b875dde1f68304faa9d78b4b833
hideonbush233-lucy <55594771+hideonbush233-lucy@users.noreply.github.com>
2021-04-11 14:35:08 +0800

    Update JZ41-和为S的连续正数序列.py

--- 剑指Offer/JZ41-和为S的连续正数序列.py
+++ 剑指Offer/JZ41-和为S的连续正数序列.py
@@ -23,3 +23,36 @@ class Solution:
                 elif tmp > tsum:
                     break
         return res
+
+# 滑动窗口法
+# 首先是一个窗口，既然是一个窗口，就需要用窗口的左边界i和右边界j来唯一表示一个窗口，其次，滑动代表，窗口始终从左往右移动，这也表明左边界i和右边界j始终会往后移动，而不会往左移动。
+
+#     初始化，i=1,j=1, 表示窗口大小为0
+#     如果窗口中值的和小于目标值tsum， 表示需要扩大窗口，j += 1
+#     否则，如果窗口值和大于目标值tsum，表示需要缩小窗口，i += 1
+#     否则，等于目标值，存结果，缩小窗口，继续进行步骤2,3,4 
+class Solution:
+    def FindContinuousSequence(self, tsum):
+        # write code here
+        if tsum == 0:
+            return []
+        elif tsum == 1:
+            return []
+        l = r = 1
+        res = []
+        tmp = 0
+        while l <= tsum//2+1:
+            if tmp < tsum:
+                tmp += r
+                r += 1
+            elif tmp > tsum:
+                tmp -= l
+                l += 1
+            else:
+                temp = []
+                for k in range(l,r):
+                    temp.append(k)
+                res.append(temp)
+                tmp -= l
+                l += 1
+        return res

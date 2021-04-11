f97634a7890dfd95f64f8121dcb0d1cbdd282fbe
hideonbush233-lucy <55594771+hideonbush233-lucy@users.noreply.github.com>
2021-04-11 15:33:57 +0800

    Create JZ44-翻转单词顺序列.py

---
+++ 剑指Offer/JZ44-翻转单词顺序列.py
@@ -0,0 +1,24 @@
+# 输入
+# "nowcoder. a am I"
+
+# 返回值
+# "I am a nowcoder."
+
+
+# split()方法 指定分隔符对字符串进行切片，如果第二个参数 num 有指定值，则分割为 num+1 个子字符串 返回分割后的字符串列表
+# reverse() 函数用于反向列表中元素 
+#  join()方法将序列中的元素以指定的字符连接生成一个新的字符串 
+# str.join(sequence)
+# sequence -- 要连接的元素序列
+
+class Solution:
+    def ReverseSentence(self, s):
+        # write code here
+        if not s:
+            return s
+        l = s.split()
+        if len(l) == 0:
+            return s
+        l.reverse()
+        news = ' '.join(l)
+        return news

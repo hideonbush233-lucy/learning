8d2a32ec8c1b8e15f1cfe65083b0e4b190c1b56f
hideonbush233-lucy <55594771+hideonbush233-lucy@users.noreply.github.com>
2021-04-13 12:54:34 +0800

    Create JZ29-最小的K个数.py

---
+++ 剑指Offer/JZ29-最小的K个数.py
@@ -0,0 +1,14 @@
+# 给定一个数组，找出其中最小的K个数。例如数组元素是4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4。
+# 如果K>数组的长度，那么返回一个空的数组
+
+# 利用sorted()排序，返回片段
+class Solution:
+    def GetLeastNumbers_Solution(self, tinput, k):
+        # write code here
+        if len(tinput) == 0:
+            return []
+        if k > len(tinput):
+            return []
+        
+        tinput = sorted(tinput)
+        return tinput[:k]

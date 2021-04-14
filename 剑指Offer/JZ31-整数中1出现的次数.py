b3bbd05333dcf9f684351588a93ba1a9d836fa37
hideonbush233-lucy <55594771+hideonbush233-lucy@users.noreply.github.com>
2021-04-14 17:33:08 +0800

    Create JZ31-整数中1出现的次数.py

---
+++ 剑指Offer/JZ31-整数中1出现的次数.py
@@ -0,0 +1,23 @@
+# 输入一个整数 n ，求1～n这n个整数的十进制表示中1出现的次数
+# 例如，1~13中包含1的数字有1、10、11、12、13因此共出现6次
+
+
+# 效率很低的方法，逐个算1的个数，然后加起来即可
+class Solution:
+    def NumberOf1Between1AndN_Solution(self, n):
+        # write code here
+        if n < 1:
+            return 0
+        res = 0
+        for i in range(1,n+1):
+            res = res + numcount(i)
+        return res
+        
+def numcount(num):
+    count = 0
+    while num > 0:
+        temp = num%10
+        if temp == 1:
+            count += 1
+        num = num//10
+    return count

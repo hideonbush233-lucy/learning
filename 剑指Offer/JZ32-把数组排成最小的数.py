f27ba36c924be61571fd9db75458e8b8935bc1d2
hideonbush233-lucy <55594771+hideonbush233-lucy@users.noreply.github.com>
2021-04-15 13:20:33 +0800

    Create JZ32-把数组排成最小的数.py

---
+++ 剑指Offer/JZ32-把数组排成最小的数.py
@@ -0,0 +1,16 @@
+# 输入一个正整数数组，把数组里所有数字拼接起来排成一个数，打印能拼接出的所有数字中最小的一个。
+# 例如输入数组{3，32，321}，则打印出这三个数字能排成的最小数字为321323。
+
+
+# 贪心，自定义排序 
+class Solution:
+    def PrintMinNumber(self, numbers):
+        # write code here
+        res = []
+        if len(numbers) == 0:
+            return ''
+        if len(numbers) == 1:
+            return numbers[0]
+        compare = lambda n1,n2:int(str(n1)+str(n2))-int(str(n2)+str(n1))
+        array = sorted(numbers,cmp = compare)
+        return ''.join([str(i) for i in array])

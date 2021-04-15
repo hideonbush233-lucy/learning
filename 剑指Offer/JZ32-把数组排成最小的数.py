6a1d30a73e6beca00be5a9ce3b2b52d2e302cb6d
hideonbush233-lucy <55594771+hideonbush233-lucy@users.noreply.github.com>
2021-04-15 15:01:48 +0800

    Update JZ32-把数组排成最小的数.py

--- 剑指Offer/JZ32-把数组排成最小的数.py
+++ 剑指Offer/JZ32-把数组排成最小的数.py
@@ -14,3 +14,32 @@ class Solution:
         compare = lambda n1,n2:int(str(n1)+str(n2))-int(str(n2)+str(n1))
         array = sorted(numbers,cmp = compare)
         return ''.join([str(i) for i in array])
+
+    
+r# 定义一个函数实现全排列，然后把字符转换为数字，排序得到最小数
+class Solution:
+    def PrintMinNumber(self, numbers):
+        # write code here
+        n = len(numbers)
+        if len(numbers) == 0:
+            return ''
+        if len(numbers) == 1:
+            return numbers[0]
+        res = permutation(numbers)
+        new_res = []
+        for i in range(len(res)):
+            tmp = int(''.join(str(num) for num in res[i]))
+            new_res.append(tmp)
+        new_res = sorted(new_res)
+        return new_res[0]
+        
+def permutation(nums):
+    if len(nums) <= 1:
+        return [nums]
+    r = []
+    for i in range(len(nums)):
+        s = nums[:i] + nums[i+1:]
+        p = permutation(s)
+        for x in p:
+            r.append(nums[i:i+1]+x)
+    return r

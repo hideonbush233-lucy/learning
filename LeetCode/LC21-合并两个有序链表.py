e8bcee11c433d701a0b893ab1a7f303abab6e289
Naughty Galileo <55594771+Naughty-Galileo@users.noreply.github.com>
2021-04-22 21:59:42 +0800

    Update LC21-合并两个有序链表.py

--- LeetCode/LC21-合并两个有序链表.py
+++ LeetCode/LC21-合并两个有序链表.py
@@ -34,3 +34,17 @@ class Solution:
         if not l2:
             cur.next = l1
         return head
+
+# 递归法
+ class Solution:
+    def mergeTwoLists(self, l1, l2):
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2

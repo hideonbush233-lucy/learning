b99050a0155b49afab64cd04b2ba8f22f94e181c
Naughty Galileo <55594771+Naughty-Galileo@users.noreply.github.com>
2021-05-08 21:24:39 +0800

    Create LC56-合并区间.py

---
+++ LeetCode/LC56-合并区间.py
@@ -0,0 +1,12 @@
+# 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x: x[0])
+        new_arr = []
+        for item in intervals:
+            if not new_arr or new_arr[-1][1] < item[0]:
+                new_arr.append(item)
+            else:
+                new_arr[-1][1] = max(new_arr[-1][1],item[1])
+        return new_arr

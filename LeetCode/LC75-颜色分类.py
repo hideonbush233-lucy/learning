ea48d02bdfb88aafb842d54f516498719edbb2f0
Naughty Galileo <55594771+Naughty-Galileo@users.noreply.github.com>
2021-05-14 17:26:45 +0800

    Create LC75-颜色分类.py

---
+++ LeetCode/LC75-颜色分类.py
@@ -0,0 +1,20 @@
+给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+
+此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+
+# 两次遍历，第一次把0全部放到最前面，然后第二次放1
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        ptr = 0
+        for i in range(n):
+            if nums[i] == 0:
+                nums[i], nums[ptr] = nums[ptr], nums[i]
+                ptr += 1
+        for i in range(ptr, n):
+            if nums[i] == 1:
+                nums[i], nums[ptr] = nums[ptr], nums[i]
+                ptr += 1

fccb361ffae26d31d7cb49aa15a5c933bb0ccfa8
Naughty Galileo <55594771+Naughty-Galileo@users.noreply.github.com>
2021-05-27 12:53:34 +0800

    Create LC128-最长连续序列.py

---
+++ LeetCode/101~199/LC128-最长连续序列.py
@@ -0,0 +1,22 @@
+# 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+
+
+# 进阶：你可以设计并实现时间复杂度为 O(n) 的解决方案吗？
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        if n <= 1:
+            return n
+        maxlen = 0
+        sum = 1
+        for i in range(1,n):
+            temp = nums[i] - nums[i-1]
+            if temp <= 1:
+                sum += temp
+            else:
+                maxlen = max(maxlen,sum)
+                sum = 1
+        return max(maxlen,sum)

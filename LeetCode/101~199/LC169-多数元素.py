99ba34297e4ec6724c963a1939935da2122b10a4
Naughty Galileo <55594771+Naughty-Galileo@users.noreply.github.com>
2021-06-07 14:12:57 +0800

    Create LC169-多数元素.py

---
+++ LeetCode/101~199/LC169-多数元素.py
@@ -0,0 +1,16 @@
+# 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+
+# 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        dic = {}
+        for num in nums:
+            if num not in dic:
+                dic[num] = 1
+            else:
+                dic[num] += 1
+            if dic[num] > n/2:
+                return num

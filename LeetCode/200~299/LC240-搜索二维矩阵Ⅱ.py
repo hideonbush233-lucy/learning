736ec531f77df71fa135a48e725c5f87a52e0dbc
Naughty Galileo <55594771+Naughty-Galileo@users.noreply.github.com>
2021-06-29 16:24:21 +0800

    Create LC240-搜索二维矩阵Ⅱ.py

---
+++ LeetCode/200~299/LC240-搜索二维矩阵Ⅱ.py
@@ -0,0 +1,20 @@
+# 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+
+#     每行的元素从左到右升序排列。
+#     每列的元素从上到下升序排列。
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
+            return False
+        m = len(matrix)
+        n = len(matrix[0])
+        i,j = m-1,0
+        while i>=0 and j<n:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] > target:
+                i -= 1
+            else:
+                j += 1
+        return False

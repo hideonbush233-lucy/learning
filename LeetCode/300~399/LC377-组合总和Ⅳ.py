37ca304e99f4a0ea1a2c337c48d9f50cbbbb60d9
Naughty Galileo <55594771+Naughty-Galileo@users.noreply.github.com>
2021-07-08 12:03:21 +0800

    Create LC377-组合总和Ⅳ.py

---
+++ LeetCode/300~399/LC377-组合总和Ⅳ.py
@@ -0,0 +1,12 @@
+# 给你一个由 不同 整数组成的数组 nums ，和一个目标整数 target 。请你从 nums 中找出并返回总和为 target 的元素组合的个数。
+
+# 题目数据保证答案符合 32 位整数范围。
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = [1] + [0] * target
+        for i in range(1, target + 1):
+            for num in nums:
+                if num <= i:
+                    dp[i] += dp[i - num]
+        
+        return dp[target]

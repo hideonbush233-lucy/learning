979473217f807432c492cd2cbba136e5220d3546
Naughty Galileo <55594771+Naughty-Galileo@users.noreply.github.com>
2021-07-11 21:24:19 +0800

    Create LC393-UTF-8编码验证.py

---
+++ LeetCode/300~399/LC393-UTF-8编码验证.py
@@ -0,0 +1,55 @@
+# UTF-8 中的一个字符可能的长度为 1 到 4 字节，遵循以下的规则：
+
+#     对于 1 字节的字符，字节的第一位设为 0 ，后面 7 位为这个符号的 unicode 码。
+#     对于 n 字节的字符 (n > 1)，第一个字节的前 n 位都设为1，第 n+1 位设为 0 ，后面字节的前两位一律设为 10 。剩下的没有提及的二进制位，全部为这个符号的 unicode 码。
+# 给定一个表示数据的整数数组，返回它是否为有效的 utf-8 编码。
+# 这是 UTF-8 编码的工作方式：
+# 注意：
+# 输入是整数数组。只有每个整数的 最低 8 个有效位 用来存储数据。这意味着每个整数只表示 1 字节的数据
+
+
+
+class Solution:
+    def validUtf8(self, data):
+        """
+        :type data: List[int]
+        :rtype: bool
+        """
+
+        # Number of bytes in the current UTF-8 character
+        n_bytes = 0
+
+        # For each integer in the data array.
+        for num in data:
+
+            # Get the binary representation. We only need the least significant 8 bits
+            # for any given number.
+            bin_rep = format(num, '#010b')[-8:]
+
+            # If this is the case then we are to start processing a new UTF-8 character.
+            if n_bytes == 0:
+
+                # Get the number of 1s in the beginning of the string.
+                for bit in bin_rep:
+                    if bit == '0': break
+                    n_bytes += 1
+
+                # 1 byte characters
+                if n_bytes == 0:
+                    continue
+
+                # Invalid scenarios according to the rules of the problem.
+                if n_bytes == 1 or n_bytes > 4:
+                    return False
+            else:
+                # Else, we are processing integers which represent bytes which are a part of
+                # a UTF-8 character. So, they must adhere to the pattern `10xxxxxx`.
+                if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
+                    return False
+
+            # We reduce the number of bytes to process by 1 after each integer.
+            n_bytes -= 1
+
+        # This is for the case where we might not have the complete data for
+        # a particular UTF-8 character.
+        return n_bytes == 0
